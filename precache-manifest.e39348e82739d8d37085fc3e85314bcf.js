5008eaa832172c4ea5345f397c17569086e8f53d
Bartosz Kalbarczyk <bartekkalmar@gmail.com>
2023-09-28 21:24:43 +0200

    Updates

---
+++ precache-manifest.e39348e82739d8d37085fc3e85314bcf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c429fe2de66de7978bfa",
+    "url": "/app-rr/static/css/main.e64ffce0.chunk.css"
+  },
+  {
+    "revision": "c429fe2de66de7978bfa",
+    "url": "/app-rr/static/js/main.957fe6da.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "2c219d3f0808b8fe09fc",
+    "url": "/app-rr/static/js/2.ca7281a5.chunk.js"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "8d802f88a96815082d513eb02a89ce44",
+    "url": "/app-rr/index.html"
+  }
+];
